f63ff3d055d6a4fbf4040ea88c6dbcdb0220a001
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2021-08-18 14:53:34 +0200

    Update test_trend.py

--- test_trend.py
+++ test_trend.py
@@ -1,10 +1,6 @@
 import unittest
-from run import create_app
 from trend import indis, oscs, patts
 
-
-
-app = create_app()
 
 class FlaskTestCase(unittest.TestCase):
 
@@ -12,40 +8,40 @@ class FlaskTestCase(unittest.TestCase):
 
     # Ensures that value of calculated indicators is not none
     def test_adx(self):
-        self.assertFalse(indis.adx is None)
+        self.assertIsNotNone(indis.adx)
 
     def test_bblow(self):
-        self.assertFalse(indis.bblow is None)
+        self.assertIsNotNone(indis.bblow)
 
     def test_dema(self):
-        self.assertFalse(indis.dema is None)
+        self.assertIsNotNone(indis.dema)
 
     def test_ema(self):
-        self.assertFalse(indis.ema is None)
+        self.assertIsNotNone(indis.ema)
 
     def test_ht(self):
-        self.assertFalse(indis.ht is None)
+        self.assertIsNotNone(indis.ht)
 
     def test_kama(self):
-        self.assertFalse(indis.kama is None)
+        self.assertIsNotNone(indis.kama)
 
     def test_fama(self):
-        self.assertFalse(indis.fama is None)
+        self.assertIsNotNone(indis.fama)
 
     def test_sar(self):
-        self.assertFalse(indis.sar is None)
+        self.assertIsNotNone(indis.sar)
 
     def test_sma(self):
-        self.assertFalse(indis.sma is None)
+        self.assertIsNotNone(indis.sma)
 
     def test_t3(self):
-        self.assertFalse(indis.t3 is None)
+        self.assertIsNotNone(indis.t3)
 
     def test_trima(self):
-        self.assertFalse(indis.trima is None)
+        self.assertIsNotNone(indis.trima)
 
     def test_wma(self):
-        self.assertFalse(indis.wma is None)
+        self.assertIsNotNone(indis.wma)
 
     # Ensures that number of items in results match the given values
     def test_indis_results_values(self):
@@ -64,25 +60,25 @@ class FlaskTestCase(unittest.TestCase):
 
     # Ensures that value of calculated oscilators is not none
     def test_cci(self):
-        self.assertFalse(oscs.cci is None)
+        self.assertIsNotNone(oscs.cci)
 
     def test_macd(self):
-        self.assertFalse(oscs.macd is None)
+        self.assertIsNotNone(oscs.macd)
 
     def test_mom(self):
-        self.assertFalse(oscs.mom is None)
+        self.assertIsNotNone(oscs.mom)
 
     def test_roc(self):
-        self.assertFalse(oscs.roc is None)
+        self.assertIsNotNone(oscs.roc)
 
     def test_rsi(self):
-        self.assertFalse(oscs.rsi is None)
+        self.assertIsNotNone(oscs.rsi)
 
     def test_stoch(self):
-        self.assertFalse(oscs.stoch is None)
+        self.assertIsNotNone(oscs.stoch)
 
     def test_willr(self):
-        self.assertFalse(oscs.willr is None)
+        self.assertIsNotNone(oscs.willr)
 
     # Ensures that number of items in results match the given values
     def test_oscs_results_values(self):
@@ -100,112 +96,112 @@ class FlaskTestCase(unittest.TestCase):
 
     # Ensures that value of calculated candlestick patterns is not none
     def test_baby(self):
-        self.assertFalse(patts.baby is None)
+        self.assertIsNotNone(patts.baby)
 
     def test_belt(self):
-        self.assertFalse(patts.belt is None)
+        self.assertIsNotNone(patts.belt)
 
     def test_b_away(self):
-        self.assertFalse(patts.b_away is None)
+        self.assertIsNotNone(patts.b_away)
 
     def test_closing(self):
-        self.assertFalse(patts.closing is None)
+        self.assertIsNotNone(patts.closing)
 
     def test_swallow(self):
-        self.assertFalse(patts.swallow is None)
+        self.assertIsNotNone(patts.swallow)
 
     def test_counter(self):
-        self.assertFalse(patts.counter is None)
+        self.assertIsNotNone(patts.counter)
 
     def test_dragon(self):
-        self.assertFalse(patts.dragon is None)
+        self.assertIsNotNone(patts.dragon)
 
     def test_engulfing(self):
-        self.assertFalse(patts.engulfing is None)
+        self.assertIsNotNone(patts.engulfing)
 
     def test_gap(self):
-        self.assertFalse(patts.gap is None)
+        self.assertIsNotNone(patts.gap)
 
     def test_hammer(self):
-        self.assertFalse(patts.hammer is None)
+        self.assertIsNotNone(patts.hammer)
 
     def test_harami(self):
-        self.assertFalse(patts.harami is None)
+        self.assertIsNotNone(patts.harami)
 
     def test_har_cross(self):
-        self.assertFalse(patts.har_cross is None)
+        self.assertIsNotNone(patts.har_cross)
 
     def test_hikkake(self):
-        self.assertFalse(patts.hikkake is None)
+        self.assertIsNotNone(patts.hikkake)
 
     def test_pigeon(self):
-        self.assertFalse(patts.pigeon is None)
+        self.assertIsNotNone(patts.pigeon)
 
     def test_kick(self):
-        self.assertFalse(patts.kick is None)
+        self.assertIsNotNone(patts.kick)
 
     def test_ladder(self):
-        self.assertFalse(patts.ladder is None)
+        self.assertIsNotNone(patts.ladder)
 
     def test_long_line(self):
-        self.assertFalse(patts.long_line is None)
+        self.assertIsNotNone(patts.long_line)
 
     def test_marubozu(self):
-        self.assertFalse(patts.marubozu is None)
+        self.assertIsNotNone(patts.marubozu)
 
     def test_ma_low(self):
-        self.assertFalse(patts.ma_low is None)
+        self.assertIsNotNone(patts.ma_low)
 
     def test_mat_hold(self):
-        self.assertFalse(patts.mat_hold is None)
+        self.assertIsNotNone(patts.mat_hold)
 
     def test_mor_doji(self):
-        self.assertFalse(patts.mor_doji is None)
+        self.assertIsNotNone(patts.mor_doji)
 
     def test_mor_star(self):
-        self.assertFalse(patts.mor_star is None)
+        self.assertIsNotNone(patts.mor_star)
 
     def test_piercing(self):
-        self.assertFalse(patts.piercing is None)
+        self.assertIsNotNone(patts.piercing)
 
     def test_three_met(self):
-        self.assertFalse(patts.three_met is None)
+        self.assertIsNotNone(patts.three_met)
 
     def test_sep_lines(self):
-        self.assertFalse(patts.sep_lines is None)
+        self.assertIsNotNone(patts.sep_lines)
 
     def test_stick(self):
-        self.assertFalse(patts.stick is None)
+        self.assertIsNotNone(patts.stick)
 
     def test_takuri(self):
-        self.assertFalse(patts.takuri is None)
+        self.assertIsNotNone(patts.takuri)
 
     def test_tasuki(self):
-        self.assertFalse(patts.tasuki is None)
+        self.assertIsNotNone(patts.tasuki)
 
     def test_inside_up(self):
-        self.assertFalse(patts.inside_up is None)
+        self.assertIsNotNone(patts.inside_up)
 
     def test_tlstrike(self):
-        self.assertFalse(patts.tlstrike is None)
+        self.assertIsNotNone(patts.tlstrike)
 
     def test_south(self):
-        self.assertFalse(patts.south is None)
+        self.assertIsNotNone(patts.south)
 
     def test_soldiers(self):
-        self.assertFalse(patts.soldiers is None)
+        self.assertIsNotNone(patts.soldiers)
 
     def test_outside_up(self):
-        self.assertFalse(patts.outside_up is None)
+        self.assertIsNotNone(patts.outside_up)
 
     def test_tristar(self):
-        self.assertFalse(patts.tristar is None)
+        self.assertIsNotNone(patts.tristar)
 
     def test_river(self):
-        self.assertFalse(patts.river is None)
+        self.assertIsNotNone(patts.river)
 
     def test_u_gap(self):
-        self.assertFalse(patts.u_gap is None)
+        self.assertIsNotNone(patts.u_gap)
 
     # Ensures that number of items in results match the given names
     def test_patts_results_names(self):
