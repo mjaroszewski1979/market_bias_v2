cbbc6949cffb894cb8d83f83949879b8f1d48a06
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2024-07-07 21:07:41 +0200

    Update strategies.py

--- strategies.py
+++ strategies.py
@@ -1,4 +1,6 @@
+# Import the get_strategies_data function from the utilities module
 from utilities import get_strategies_data
+# Import the indis object from the trend module
 from trend import indis
 
 
@@ -6,10 +8,14 @@ from trend import indis
 if indis.error == None:
 
     class Indices:
-
+        # Class to represent and manage financial indices
         def __init__(self):
+            # Initialize the Indices object
 
-            self.data = get_strategies_data()
+            # Fetch strategies data
+            self.data = get_strategies_data() 
+            
+            # Dictionary to store the latest adjusted close prices of various indices
             self.indices = {
                 'US/S&P 500' : self.data['Adj Close']['^GSPC'][-1],
                 'US/NASDAQ' : self.data['Adj Close']['^IXIC'][-1],
@@ -33,11 +39,13 @@ if indis.error == None:
                 'SOUTH AFRICA/MSCI' : self.data['Adj Close']['EZA'][-1],
                 'UK/MSCI' : self.data['Adj Close']['EWU'][-1]
             }
-            
+
+            # Create a sorted list of tuples with index values and names, sorted in descending order
             self.top_index = sorted([(v, k) for k, v in self.indices.items()], reverse=True)
 
+    # Create an instance of the Indices class
     indices = Indices()
 
 else:
-
-    pass
+    # If there is an error, print its message
+    print(error)
