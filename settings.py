c2fb50322c15e40baa022f631d6dd44b3590c6fa
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2024-07-07 21:02:29 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
+# Import the environ module from the os package to access environment variables
 from os import environ
 
+# Retrieve the SECRET_KEY from environment variables for secure configurations
 SECRET_KEY = environ.get('SECRET_KEY')
